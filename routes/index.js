b2e3738b6c8aa17ea6366ff9d6ac7a03d65e5ff9
paposeco <13892562+paposeco@users.noreply.github.com>
2023-02-06 18:16:26 +0000

    it works. no css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Oi",
+    user: "Estela",
+    added: new Date()
+  },
+  {
+    text: "Ola!",
+    user: "Tome",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: "Mini Messageboard", messages: messages})
 });
+
+/* get new message page */
+router.get('/new', function(req, res, next){
+  res.render('form', {title: "New Message"})
+})
+
+router.post('/new', function(req, res, next){
+  const newmessage = {text: req.body.message, user: req.body.username, added: new Date()};
+  messages.push(newmessage);
+  res.redirect('/');
+})
 
 module.exports = router;
